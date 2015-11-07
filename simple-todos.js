90393882286273748e3de7c1dae3b42815828f56
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-11-06 19:52:33 -0500

    Add create and delete,through tutorial step 5

--- simple-todos.js
+++ simple-todos.js
@@ -1,9 +1,42 @@
+Tasks = new Mongo.Collection("tasks");;
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks : [
-    {text : "test1"},
-    {text : "test2"},
-    {text : "test3"}]
+  	tasks: function() {
+	   	// Show the newest at the top
+		  return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+	"submit .new-task": function (event) {
+		//Prevent default browser form submit
+		event.preventDefault();
+
+		//Get value from form element
+		var text = event.target.text.value;
+
+		// Insert a task into the collection
+		Tasks.insert({
+			text: text,
+			createdAt: new Date() //current time
+		});
+
+		// Clear form
+		event.target.text.value = "";
+	}
+  });
+
+  Template.task.events({
+	 "click .toggle-checked": function () {
+		  //Set the checked property to the opposite of its current value
+		  Tasks.update(this._id, {
+		  	$set: {checked: ! this.checked}
+	 	 });
+	 },
+	 "click .delete": function () {
+		  Tasks.remove(this._id);
+    } 
   });
 }
 
